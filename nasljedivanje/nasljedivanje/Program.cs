3cbf5131176da951425adc8890b5f125dbcd4e1a
SeadColakovic <flegarsead263@gmail.com>
2025-03-03 12:41:25 +0100

    Add files via upload

---
+++ nasljedivanje/nasljedivanje/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nasljedivanje
+{
+    public class Dessert
+    {
+        private string name;
+        private double weight;
+        private int calories;
+
+        public Dessert(string name, double weight, int calories)
+        {
+            this.name = name;
+            this.weight = weight;
+            this.calories = calories;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public double Weight
+        {
+            get { return weight; }
+            set { weight = value; }
+        }
+
+        public int Calories
+        {
+            get { return calories; }
+            set { calories = value; }
+        }
+
+        public override string ToString()
+        {
+            return $"Dessert: {name}, Weight: {weight}, Calories: {calories}";
+        }
+
+        public string GetDessertType()
+        {
+            return "dessert";
+        }
+    }
+
+}
